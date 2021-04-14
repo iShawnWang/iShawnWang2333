84aa947d4df88201742c769b90e470c83016f7eb
iShawnWang <iShawnWang2333@gmail.com>
2021-04-14 13:10:07 +0800

    test

--- common/F.ts
+++ common/F.ts
@@ -1,7 +1,2 @@
 export const F = (url: string, init?: RequestInit): Promise<Response> =>
-  fetch(
-    url.startsWith('http')
-      ? url
-      : `${process.env.BASE_URL ?? 'http://localhost:3000'}${url}`,
-    init
-  )
+  fetch(url.startsWith('http') ? url : `${process.env.BASE_URL}${url}`, init)
